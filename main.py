6f35d16c5b18fc93b2bf5c14ee88b494b6b494ee
Paweł Tymosiak <pawel.tymosiak@itechart-group.com>
2022-12-01 17:10:24 +0100

    Created basic FastAPI app and pipfile

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from uvicorn import run
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root() -> dict:
+    """Only endpoint with simple JSON response."""
+    return {"status": "OK"}
+
+if __name__ == "__main__":
+    run("main:app", port=5000, log_level="info")
