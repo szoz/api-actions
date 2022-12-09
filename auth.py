3772110afa249a23a595394b7902113c95df1ac2
Paweł Tymosiak <pawel.tymosiak@itechart-group.com>
2022-12-09 15:12:26 +0100

    Added token auth for products endpoint Added JWT creation on /login endpoint and token restriction for /products. Fixed docker-compose port.

--- auth.py
+++ auth.py
@@ -1,19 +1,30 @@
+from os import environ
+from time import time
+from secrets import token_hex
+
 from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBasic, HTTPBasicCredentials, HTTPBearer, HTTPAuthorizationCredentials
 
 from sqlalchemy.orm import Session
 
 from bcrypt import checkpw
+from jwt import encode, decode
+from jwt.exceptions import InvalidTokenError
 
 from database import get_db_session
 from models import User
 
-security = HTTPBasic()
+JWT_SECRET = environ.get('JWT_SECRET', token_hex(24))
+JWT_ALGORITHM = 'HS256'
+JWT_EXPIRE = 15 * 60
+
+security_basic = HTTPBasic()
+security_token = HTTPBearer()
 
 
-def login_required(credentials: HTTPBasicCredentials = Depends(security),
-                   db: Session = Depends(get_db_session)):
-    """Verify if valid credentials are provided in the requests (basic auth)."""
+def login_required(credentials: HTTPBasicCredentials = Depends(security_basic),
+                   db: Session = Depends(get_db_session)) -> None:
+    """Verify if valid credentials are provided in the request (basic auth)."""
     user = db.query(User).filter(User.name == credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
@@ -21,3 +32,22 @@ def login_required(credentials: HTTPBasicCredentials = Depends(security),
     is_pass_ok = checkpw(credentials.password.encode('utf8'), user.password)
     if not is_pass_ok:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+def generate_token_payload_response(credentials: HTTPBasicCredentials = Depends(security_basic)) -> dict:
+    """Generate JWT for given user credentials"""
+    payload = {'user': credentials.username,
+               'exp': time() + JWT_EXPIRE}
+    token = encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return {'access_token': token,
+            'expires_in': JWT_EXPIRE}
+
+
+def token_required(credentials: HTTPAuthorizationCredentials = Depends(security_token)) -> None:
+    """Verify if valid token is provided in the request."""
+    token = credentials.credentials
+    try:
+        decode(token, JWT_SECRET, algorithms=JWT_ALGORITHM)
+    except InvalidTokenError as exc:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc))
